fe32586019264d4791f6a67be232c95ed5a13104
Germankuchen <germankuchen@gmail.com>
2022-03-26 16:24:40 -0300

    last

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
 import Server from './clases/server';
-import { SERVER_PORT } from './global/environment';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
+// Se inicializa el servidor
 const server = new Server();
+
+// Se configura el body parser
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+// Se configura el cors
+server.app.use(cors({origin: true, credentials: true}));
+
+// Se mandan las rutas
+server.app.use('/', router);
 
 server.start(() => {
     console.log('El servidor esta corriendo en el puerto ' + server.port);
